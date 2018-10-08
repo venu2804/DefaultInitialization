b2421a56a1a808a5a970338a2b9a9bdc898a0cc9
venu2804 <venu2804@gmail.com>
2018-10-08 20:06:44 +0100

    first commit

---
+++ src/academy/learnProgramming/DefaultInitialization.java
@@ -0,0 +1,30 @@
+package academy.learnProgramming;
+
+public class DefaultInitialization {
+   static boolean myBoolean;
+   static int myInt;
+   static short myShort;
+   static long myLong;
+   static float myFloat;
+   static double myDouble;
+   static char myChar;
+
+    public static void main(String[] args) {
+
+        //boolean myLocalBoolean;
+        //System.out.println("My Boolean = " + myLocalBoolean);
+
+
+        System.out.println("My Boolean = " + myBoolean);
+        System.out.println("My Int = " + myInt);
+        System.out.println("My Long = " + myLong);
+        System.out.println("My Short = " + myShort);
+        System.out.println("My Float = " + myFloat);
+        System.out.println("My Double = " + myDouble);
+        System.out.println("My Char = " + myChar);
+
+
+
+    }
+
+}
